d9bf1cf758e3da31532779f06dfc43b6ebe14dcf
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-25 20:54:47 +0200

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleCalculator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Assembly .dll or .exe files default to project name
+        //Microsoft suggests to consider the fomat of the assembly name as <company>.<product>.<module (dll or exe)>
+        //Namespace Globomantics.Data.DTO
+        //Assembly Globomantics.Data.Entity
+        //Assembly Globomantics.Data.Validation
+        //Class library Project: Globomantics.Data > Globomantics.Data.dll
+    }
+}
